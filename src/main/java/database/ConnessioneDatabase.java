4ce8fc029c66ad797e4d8fde73695a74ade66042
pagliuca26 <robertatrotti08@libero.it>
2026-09-10 10:28:53 +0200

    backupDB

--- src/main/java/database/ConnessioneDatabase.java
+++ src/main/java/database/ConnessioneDatabase.java
@@ -11,7 +11,7 @@ public class ConnessioneDatabase {
 	public Connection connection = null;
 	private String nome = "postgres";
 	private String password = "password";
-	private String url = "jdbc:postgresql://localhost:5432/boxy_db";
+	private String url = "jdbc:postgresql://localhost:5432/Boxy";
 	private String driver = "org.postgresql.Driver";
 
 	// COSTRUTTORE
